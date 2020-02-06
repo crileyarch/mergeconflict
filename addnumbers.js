3c4514387fa2ab6eb3a73290a03066191e61b4c9
Chris Riley <chris.riley@cprime.com>
2020-02-05 21:48:30 -0500

    initial commit

---
+++ addnumbers.js
@@ -0,0 +1,5 @@
+function addnumbers(val1,val2){
+	//add two numbers and return result
+	let result = val1 + val2;
+	return result;
+}
---
+++ subtractnumbers.js
@@ -0,0 +1,5 @@
+function subtractnumbers(val1,val2){
+	//subtract two numbers and return result
+	let result = val1 - val2;
+	return result;
+}
